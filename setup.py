05d3fd382f0fc1437dbb697975115f605555f5fb
James Polley <jp@jamezpolley.com>
2015-05-21 16:37:17 -0700

    Modernise testing with testr and tox
    This enables easier testing across multiple versions of python.
    
    Using this, I've been able to confirm that 117 tests pass on py26 and
    py27, with 122 passing on py3.3 and py3.4 (I don't have py3.3 available)
    
    Travis config is now done mostly automatically via tox-travis

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 from setuptools import setup
-import re
 import sys
-
-PYTHON3K = sys.version_info[0] > 2
 
 setup(
     setup_requires=['pbr'],
     pbr=True,
-    tests_require=['mock'] + ([] if PYTHON3K else ['unittest2']),
-    test_suite="tests" if PYTHON3K else "unittest2.collector"
 )
